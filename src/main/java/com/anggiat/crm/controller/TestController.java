508e46fddda9235f82523820b3192a1fdc632b57
anggiat0304 <anggiatpangaribuan28@gmail.com>
2025-01-09 01:49:06 +0700

    takeout jacoco

--- src/main/java/com/anggiat/crm/controller/TestController.java
+++ src/main/java/com/anggiat/crm/controller/TestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/crm")
 public class TestController {
     @GetMapping("/test")
-    public ResponseEntity<?> helloWorld(){
+    public ResponseEntity<String> helloWorld(){
         return new ResponseEntity<>("Hello World",HttpStatus.OK);
     }
 }
