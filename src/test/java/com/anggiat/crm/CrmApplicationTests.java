d8475a97f44a3522502e3cbcb45dc2bd8e98ae82
anggiat0304 <anggiatpangaribuan28@gmail.com>
2025-01-10 01:10:28 +0700

    perubahan port  8080 -> 8070

--- src/test/java/com/anggiat/crm/CrmApplicationTests.java
+++ src/test/java/com/anggiat/crm/CrmApplicationTests.java
@@ -1,6 +1,8 @@
 package com.anggiat.crm;
 
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,12 +14,7 @@ class CrmApplicationTests {
     @Test
     void contextLoads() {
         CrmApplication.main(new String[] {});
-        assertEquals("1","1");
+        assertThat("anggiat").isEqualTo("anggiat");
     }
-//    @Test
-//    void mainMethodTest() {
-//        // Simulasi pemanggilan metode main
-//        CrmApplication.main(new String[] {});
-//    }
 
 }
